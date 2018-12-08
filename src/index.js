1c47f47b170f7aae2cf102e04eaf38daaccc9156
喻淞彬 <songbin.yu@tongdun.cn>
2018-12-08 10:29:51 +0800

    初始化项目

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import Table from './table';
+// import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<Table />, document.getElementById('root'));
