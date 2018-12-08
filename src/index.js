e5afa1266d9096cf4d26d4265c72f94e4994b7cf
喻淞彬 <songbin.yu@tongdun.cn>
2018-12-08 15:42:26 +0800

    测试less-loader

--- src/index.js
+++ src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import './index.css';
-<<<<<<< HEAD
+// import Table from './components/table/index';
 import Table from './table';
 // import * as serviceWorker from './serviceWorker';
-
-=======
-import Table from './components/table/index';
-// import * as serviceWorker from './serviceWorker';
-console.log('ReactDOM.render')
->>>>>>> c9f2702adacea16feca247f764eacd23c811c7ea
+// console.log('ReactDOM.render')
 ReactDOM.render(<Table />, document.getElementById('root'));
--- src/table.js
+++ src/table.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import "./index.less";
 
 class Table extends Component {
   render() {
     console.log('table')
     return (
       <div className="App">
-        y - table 
+        <div className="App-con">y - table </div>
       </div>
     );
   }
