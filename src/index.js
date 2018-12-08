c9f2702adacea16feca247f764eacd23c811c7ea
lk <lk@lktekiMacBook-Air.local>
2018-12-08 15:26:32 +0800

    table

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import Table from './components/table/index';
+// import * as serviceWorker from './serviceWorker';
+console.log('ReactDOM.render')
+ReactDOM.render(<Table />, document.getElementById('root'));
