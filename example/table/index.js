d9ba0f98efe81846febb7e3dad29d4013e0cf86b
喻淞彬 <songbin.yu@tongdun.cn>
2019-01-03 18:05:33 +0800

    代码格式规范

--- example/table/index.js
+++ example/table/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import Table from "../../components/table";
-import Input from "../../components/input";
-import Button from "../../components/button";
-import Icon from "../../components/icon";
+import Table from '../../components/table';
+import Input from '../../components/input';
+import Button from '../../components/button';
+import Icon from '../../components/icon';
 import Highlighter from 'react-highlight-words';
 
 const data = [{
@@ -27,58 +27,65 @@ const data = [{
   address: 'London No. 2 Lake Park',
 }];
 
-export default class TableExample extends React.Component {
+export default class TableExample extends Component {
   state = {
     searchText: '',
   };
 
-  getColumnSearchProps = (dataIndex) => ({
-    filterDropdown: ({
-      setSelectedKeys, selectedKeys, confirm, clearFilters,
-    }) => (
-        <div className="custom-filter-dropdown">
-          <Input
-            ref={node => { this.searchInput = node; }}
-            placeholder={`Search ${dataIndex}`}
-            value={selectedKeys[0]}
-            onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-            onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Button
-            type="primary"
-            onClick={() => this.handleSearch(selectedKeys, confirm)}
-            icon="search"
-            size="small"
-            style={{ width: 90, marginRight: 8 }}
-          >
-            Search
-        </Button>
-          <Button
-            onClick={() => this.handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-        </Button>
-        </div>
+  getColumnSearchProps = (dataIndex) => {
+    return ({
+      filterDropdown: ({
+        setSelectedKeys, selectedKeys, confirm, clearFilters,
+      }) => (
+          <div className="custom-filter-dropdown">
+            <Input
+              ref={(node) => { this.searchInput = node; }}
+              placeholder={`Search ${dataIndex}`}
+              value={selectedKeys[0]}
+              onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+              onPressEnter={() => this.handleSearch(selectedKeys, confirm)}
+              style={{ width: 188, marginBottom: 8, display: 'block' }}
+            />
+            <Button
+              type="primary"
+              onClick={() => this.handleSearch(selectedKeys, confirm)}
+              icon="search"
+              size="small"
+              style={{ width: 90, marginRight: 8 }}
+            >
+              Search
+            </Button>
+            <Button
+              onClick={() => this.handleReset(clearFilters)}
+              size="small"
+              style={{ width: 90 }}
+            >
+              Reset
+            </Button>
+          </div>
       ),
-    filterIcon: filtered => <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
-    onFilter: (value, record) => record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    onFilterDropdownVisibleChange: (visible) => {
-      if (visible) {
-        setTimeout(() => this.searchInput.select());
-      }
-    },
-    render: (text) => (
-      <Highlighter
-        highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
-        searchWords={[this.state.searchText]}
-        autoEscape
-        textToHighlight={text.toString()}
-      />
-    ),
-  })
+      filterIcon: filtered => <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />,
+      onFilter: (value, record) => {
+        return record[dataIndex].toString().toLowerCase().includes(value.toLowerCase());
+      },
+      onFilterDropdownVisibleChange: (visible) => {
+        if (visible) {
+          setTimeout(() => this.searchInput.select());
+        }
+      },
+      render: (text) => {
+        const { searchText } = this.state;
+        return (
+          <Highlighter
+            highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+            searchWords={[searchText]}
+            autoEscape
+            textToHighlight={text.toString()}
+          />
+        );
+      },
+    });
+  }
 
   handleSearch = (selectedKeys, confirm) => {
     confirm();
@@ -111,9 +118,8 @@ export default class TableExample extends React.Component {
     }];
     return (
       <div>
-        <Table columns={columns} dataSource={data} />;
+        <Table columns={columns} dataSource={data} />
       </div>
     );
   }
 }
-
